d65adcd0dd65635b268b34f3f315cc549ce22b16
Gabri <gabripag@outlook.com>
2024-02-21 20:10:13 +0100

    User changes

--- src/main/java/com/develhope/spring/User/Entities/User.java
+++ src/main/java/com/develhope/spring/User/Entities/User.java
@@ -1,4 +1,22 @@
 package com.develhope.spring.User.Entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
 public class User {
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false,unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
 }

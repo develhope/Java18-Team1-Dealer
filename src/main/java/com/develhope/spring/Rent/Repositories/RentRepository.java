4622a6d661279894a08f1bc3bab131ccc8f3b37e
SimoneLiotta <simoneliotta99@gmail.com>
2024-02-26 18:40:11 +0100

    aggiunta annotazioni repo su purchase e rent

--- src/main/java/com/develhope/spring/Rent/Repositories/RentRepository.java
+++ src/main/java/com/develhope/spring/Rent/Repositories/RentRepository.java
@@ -2,6 +2,8 @@ package com.develhope.spring.Rent.Repositories;
 
 import com.develhope.spring.Rent.Entities.Rent;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RentRepository extends JpaRepository<Long, Rent> {
 }
